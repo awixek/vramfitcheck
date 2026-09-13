3714ce3c01a7f899e1b8c59738af740832dc76b6
Abhishek kumar <yadavramnaresh435@gmail.com>
2026-09-13 10:43:20 +0530

    Delete apps/web/app/model/[...modelId] directory

--- apps/web/app/model/[...modelId]/page.tsx
+++
@@ -1,2 +0,0 @@
-import {ModelDetail} from "../../../components/ModelDetail";
-export default async function Page({params}:{params:Promise<{modelId:string[]}>}){const p=await params; const id=p.modelId.join("/"); return <ModelDetail modelId={id}/>;}
