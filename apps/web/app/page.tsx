dd4b4dce19f789db96f6018b86d63a9c4001c06e
Abhishek kumar <yadavramnaresh435@gmail.com>
2026-09-13 10:49:17 +0530

    Add new GPU page component

--- apps/web/app/page.tsx
+++
@@ -1,2 +0,0 @@
-import {GPUProfile} from "../../../components/GPUProfile";
-export default async function Page({params}:{params:Promise<{id:string}>}){const p=await params; return <GPUProfile id={p.id}/>;}
